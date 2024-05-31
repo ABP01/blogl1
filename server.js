07c341e43e925101e591a7f35a292259078ef3b8
Armel Bogue <komlanbogue@gmail.com>
2024-05-31 21:50:14 +0000

    First commit server file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+let initial_path = path.join(__dirname, "public");
+
+const app = express();
+app.use(express.static(initial_path));
+
