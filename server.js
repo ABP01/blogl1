6eecae04a602ca468a2e44ad505ccbfc8daa677a
Armel Bogue <komlanbogue@gmail.com>
2024-05-31 23:00:14 +0000

    "Added home.css file, deleted multiple image files, and updated server.js file to serve home.html"

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ let initial_path = path.join(__dirname, "public");
 const app = express();
 app.use(express.static(initial_path));
 
-app.get('/', (req,))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, 'home.html'));
+})
+
+app.listen("3000", () => {
+    console.log("Listening..............");
+})
