52c12a5d57a2b3ec65abba64f7eaed6b50921e5a
Armel Bogue <komlanbogue@gmail.com>
2024-06-01 08:50:05 +0000

    "Added Firebase configuration credentials and initialized Firebase app"

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,8 +1,12 @@
-
 let firebaseConfig = {
-    // Enter your firebase credentials
+    apiKey: "AIzaSyBF9hR1HnrnH-b9BIBI4r3MlhkcrXV_8hQ",
+    authDomain: "iail1-blogging.firebaseapp.com",
+    projectId: "iail1-blogging",
+    storageBucket: "iail1-blogging.appspot.com",
+    messagingSenderId: "21550890090",
+    appId: "1:21550890090:web:069ba4b8296a070fc99e13",
+    measurementId: "G-SBLL4VN4X3"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-let db = firebase.firestore();
